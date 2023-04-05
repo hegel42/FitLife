b0390fbfe373c8269e50eb880a10bf3829d95501
hegel42 <olegrudencopersonal@gmail.com>
2023-04-05 14:53:59 +0300

    modified color pallete

--- lib/app/ui_kit/models/color_palette.dart
+++ lib/app/ui_kit/models/color_palette.dart
@@ -4,10 +4,19 @@ class ColorPallete {
   static Color get primary => const Color(0xFF292D32);
 
   static Color get white => const Color(0xFFFFFFFF);
+  //rgb(246,69,39)
 
-  static Color get accent => const Color(0xFFC8AE7E);
+  static Color get accent => const Color(0xFFF64527);
 
-  static Color get lightGrey => const Color(0xFFD3D3D3);
+  static Color get orange => const Color(0xfff97337);
+
+  static Color get darkBlue => const Color(0xff103f90);
+
+  static Color get lightBlue => const Color(0xff35b6e9);
+
+  static Color get bgBlue1 => const Color(0xff3f7cb4);
+
+  static Color get bgBlue2 => const Color(0xff148ed6);
 
   static Color get silver => const Color(0xFFA8A8A8);
 
